d202523c76eab1a3fb8c89c8e4ac98ee4acc9923
+ <+>
2023-03-11 13:35:25 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(name, value) {
+    const message = []
+    for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${value} gift!`);
+    }
+    return message;
+};
+
+function countDown(i) {
+    while(i >= 0) {
+        console.log(i);
+        i--;
+    };
+};
